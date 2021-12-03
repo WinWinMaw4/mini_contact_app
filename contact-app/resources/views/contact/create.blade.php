e8f2be7577344697cb38aa6676399072104b8976
WinWinMaw4 <mawinwinmaw4@gmail.com>
2021-12-03 10:28:34 +0630

    1.1

--- contact-app/resources/views/contact/create.blade.php
+++ contact-app/resources/views/contact/create.blade.php
@@ -6,20 +6,84 @@
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>create</title>
-</head>
+    <style>
+        *{
+            margin: 0;
+            padding: 0;
+            box-sizing: border-box;
+        }
+        body{
+            background-color: #2d3748;
+            color: #f7fafc;
+            width: 100%;
+            height: 100vh;
+            display: flex;
+            justify-content: center;
+            align-items: center;
+        }
+        a{
+            color: #ff8600;
+        }
+        .card{
+            background-color: #6b7280;
+            width: fit-content;
+            height: 500px;
+            border-radius: 5px;
+            border-left: 20px solid #427d7e;
+            padding: 10px 15px;
+            /*box-shadow: -2px 2px 2px #437676, -4px 5px 3px 2px #1e5858, -6px 7px 7px 2px #063f3f,-3px 13px 4px 0px #0c4b4b, 20px 19px 20px 2px #0f0f0f8a;*/
+            box-shadow: -2px 2px 2px #437676,
+            -4px 5px 3px 2px #1e5858,
+            -6px 7px 7px 2px #063f3f,
+            -3px 13px 4px 0px #0c4b4b,
+            13px 6px 20px 0px #0f0f0f8a;
+        }
+        .card-title{
+            text-align: center;
+        }
+        .save{
+            padding: 5px;
+            margin-bottom: 2px;
+            background: #54aaa0;
+            border: none;
+            border-radius: 2px;
+            color: #ffffff;
+        }
+        input{
+            padding: 5px;
+            background: #ffffff;
+            border-radius: 2px;
+            border: 1px solid #54aaa0;
+            color: white;
+            caret-color: #0c4b4b;
+        }
+        input:focus-visible {
+            outline-offset: 0px;
+            outline-color: #54aaa0;
+        }
+    </style>
+    </head>
 <body>
-<h1>Create Contact App</h1>
-<form action="{{route('store')}}" method="post">
-    @csrf
-    <input type="text" name="name" placeholder="Contact name">
-    <input type="number" name="phone" placeholder="phone number">
-    <button>Save</button>
-</form>
-@if(session('status'))
-    <small>{{session('status')}}</small>
-@endif
+<div class="card">
+    <div class="card-title">
+        <h1>Create Contact App</h1>
+    </div><br>
+    <div class="card-body">
+        <form action="{{route('store')}}" method="post" class="form">
+            @csrf
+            <input type="text" name="name" placeholder="Contact name">
+            <input type="number" name="phone" placeholder="phone number">
+            <button class="save">Save</button>
+        </form>
+        @if(session('status'))
+            <small>{{session('status')}}</small>
+        @endif
 
-@include('contact.list')
+        @include('contact.list')
+    </div>
+</div>
+
+<img src="{{asset('img/pen1.png')}}" alt="">
 
 </body>
 </html>
--- contact-app/resources/views/contact/list.blade.php
+++ contact-app/resources/views/contact/list.blade.php
@@ -1,12 +1,24 @@
-<ul>
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css" integrity="sha512-Fo3rlrZj/k7ujTnHg4CGR2D7kSs0v4LLanw2qksYuRlEzO+tcaEPQogQ0KaoGN26/zrn20ImR1DfuLWnOo7aBA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+<br>
+<hr>
+<br>
+<h4>Contact List</h4>
+<table style="border: 2px solid;width: 100%;text-align: center;border-radius: 5px">
+    <tr style="border: 1px dotted white;background-color: #315c62">{{-- #54aaa0,#315c62--}}
+        <th>Name</th>
+        <th>Phone</th>
+        <th>Control</th>
+    </tr>
     @foreach(\App\Models\Contact::all() as $contact)
-        <li>
-            [<a href="{{route('edit',$contact->id)}}">edit</a>,
-            <form action="{{route('destory',$contact->id)}}" style="display: inline" method="post">
-                @csrf
-                @method('delete');
-                <button>Delete</button>
-            </form>]{{$contact->name}}{{$contact->phone}}
-        </li>
+        <tr style="text-align: center;">
+            <td>{{$contact->name}}</td>
+            <td>{{$contact->phone}}</td>
+            <td> <a href="{{route('edit',$contact->id)}}" class="edit"><i class="fas fa-edit"></i></a> |
+                <form action="{{route('destory',$contact->id)}}" style="display: inline" method="post">
+                    @csrf
+                    @method('delete')
+                    <button class="delete" style="border:none;background: none;color: #aa0019;cursor: pointer"><i class="fas fa-trash-alt"></i></button>
+                </form></td>
+        </tr>
     @endforeach
-</ul>
+</table>
